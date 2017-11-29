f2dbc4817791fdc9b68d95efacec018a33e66268
Aiden <havenden@gmail.com>
2017-11-29 11:25:39 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::any('/wechat', 'WeChatController@serve');
 
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/home', 'HomeController@index')->name('home');
