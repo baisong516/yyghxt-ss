6b2d030743bd926be7516ed1f2145e7d57b6aed2
chen <1098003693@qq.com>
2018-02-21 10:43:22 +0800

    '-'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.base')
 
 @section('content')
+    <link href="https://cdn.bootcss.com/photoswipe/4.1.2/photoswipe.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/photoswipe/4.1.2/default-skin/default-skin.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/photoswipe/4.1.2/photoswipe.min.js"></script>
+    <script src="https://cdn.bootcss.com/photoswipe/4.1.2/photoswipe-ui-default.min.js"></script>
     <div class="col-sm-12">
         <div class="box box-solid">
             <div class="box-header">
@@ -344,6 +348,73 @@
             </div>
         </div>
     </div>
+    <button id="btn">Open PhotoSwipe</button>
+
+    <!-- Root element of PhotoSwipe. Must have class pswp. -->
+    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+        <!-- Background of PhotoSwipe.
+             It's a separate element, as animating opacity is faster than rgba(). -->
+        <div class="pswp__bg"></div>
+
+        <!-- Slides wrapper with overflow:hidden. -->
+        <div class="pswp__scroll-wrap">
+
+            <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
+            <div class="pswp__container">
+                <!-- don't modify these 3 pswp__item elements, data is added later on -->
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+            </div>
+
+            <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+            <div class="pswp__ui pswp__ui--hidden">
+
+                <div class="pswp__top-bar">
+
+                    <!--  Controls are self-explanatory. Order can be changed. -->
+
+                    <div class="pswp__counter"></div>
+
+                    <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+                    <button class="pswp__button pswp__button--share" title="Share"></button>
+
+                    <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+                    <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+                    <!-- Preloader demo https://codepen.io/dimsemenov/pen/yyBWoR -->
+                    <!-- element will get class pswp__preloader--active when preloader is running -->
+                    <div class="pswp__preloader">
+                        <div class="pswp__preloader__icn">
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                    <div class="pswp__share-tooltip"></div>
+                </div>
+
+                <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+                </button>
+
+                <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+                </button>
+
+                <div class="pswp__caption">
+                    <div class="pswp__caption__center"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 @endsection
 
 @section('javascript')
@@ -362,22 +433,47 @@
             $('.table-head').on('click',function () {
                 var nodeId=$(this).attr('data-id');
                 var node = document.getElementById(nodeId);
-                // domtoimage.toPng(node)
-                //     .then(function (dataUrl) {
-                //         alert('ddd');
-                //     });
-                domtoimage.toSvg(node).then(function (dataUrl) {
-                        $.ajax({
-                            url:'/home/uploadimage',
-                            type:'post',
-                            data:{'imgData':dataUrl,'_token': $('input[name=_token]').val()},
-                            success:function (data) {
-                                // console.log(data);
-                                // alert(data);
-                                window.location.href=data;
-                            }
-                        });
+                domtoimage.toSvg(node,{bgcolor: '#fff'})
+                    .then(function (dataUrl) {
+                        var openPhotoSwipe = function() {
+                            var pswpElement = document.querySelectorAll('.pswp')[0];
+                            // build items array
+                            var items = [
+                                {
+                                    src: dataUrl,
+                                    w: 300,
+                                    h: 50,
+                                }
+                            ];
+                            // define options (if needed)
+                            var options = {
+                                // history & focus options are disabled on CodePen
+                                history: false,
+                                focus: false,
+                                showAnimationDuration: 0,
+                                hideAnimationDuration: 0
+                            };
+                            var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+                            gallery.init();
+                        };
+                        openPhotoSwipe();
+                        // document.getElementById('btn').onclick = openPhotoSwipe;
+                        // var img = new Image();
+                        // img.src = dataUrl;
+                        // document.body.appendChild(img);
                     });
+                // domtoimage.toSvg(node).then(function (dataUrl) {
+                //         $.ajax({
+                //             url:'/home/uploadimage',
+                //             type:'post',
+                //             data:{'imgData':dataUrl,'_token': $('input[name=_token]').val()},
+                //             success:function (data) {
+                //                 // console.log(data);
+                //                 // alert(data);
+                //                 window.location.href=data;
+                //             }
+                //         });
+                //     });
             });
         });
 
