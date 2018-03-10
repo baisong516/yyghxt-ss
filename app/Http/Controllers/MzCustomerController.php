ea91edadaa242433b0689a4bac2908cd2d8bfd61
chen <1098003693@qq.com>
2018-03-10 17:57:14 +0800

    '-'

--- app/Http/Controllers/MzCustomerController.php
+++ app/Http/Controllers/MzCustomerController.php
@@ -21,7 +21,7 @@ class MzCustomerController extends Controller
             return view('mzcustomer.read',[
                 'pageheader'=>'患者',
                 'pagedescription'=>'列表',
-                'customers'=>ZxCustomer::getCustomers(800),
+                'customers'=>ZxCustomer::getCustomers(500),
                 'users'=>Aiden::getAllUserArray(),
                 'offices'=>Aiden::getAllModelArray('offices'),
                 'diseases'=>Aiden::getAllModelArray('diseases'),
