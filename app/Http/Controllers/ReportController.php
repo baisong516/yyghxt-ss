41df5d456e3595d38196d7748088705cbe0f0ef0
chen <1098003693@qq.com>
2018-03-16 11:47:24 +0800

    '-'

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -275,7 +275,7 @@ class ReportController extends Controller
                             $report=new Report();
                             $report->office_id=intval($office_id);
                             $report->source_id=intval($source_id);
-                            $report->type=intval($type);
+                            $report->type=$type;
                             $report->type_id=intval($type_id);
                             $report->cost=(float)$cost;
                             $report->show=intval($show);
