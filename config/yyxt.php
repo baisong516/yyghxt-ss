0422b095786ef6df38134bbc16bf97b08353216c
chen <1098003693@qq.com>
2018-04-12 11:00:55 +0800

    '-'

--- config/yyxt.php
+++ config/yyxt.php
@@ -6,17 +6,17 @@ return [
     'res'=>[
         //cdn资源
         'font_awesome'=>'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        'bootstrap_css'=>'/css/bootstrap.min.css',
-        'bootstrap_js'=>'/js/bootstrap.min.js',
-        'jquery'=>'/js/jquery.min.js',
+        'bootstrap_css'=>'https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        'bootstrap_js'=>'https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js',
+        'jquery'=>'https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js',
         'admin_css'=>'https://ssyl.oss-cn-shenzhen.aliyuncs.com/res/css/AdminLTE.min.css',
-        'admin_js'=>'/js/adminlte.min.js',
+        'admin_js'=>'https://cdn.bootcss.com/admin-lte/2.4.2/js/adminlte.min.js',
         //主题皮肤
-        'admin_skin'=>'/css/skin-green.min.css',
+        'admin_skin'=>'https://cdn.bootcss.com/admin-lte/2.4.2/css/skins/skin-green.min.css',
         'app_skin'=>'skin-green',
         //兼容
-        'html5shiv'=>'/js/html5shiv.min.js',
-        'respond'=>'/js/respond.min.js',
+        'html5shiv'=>'https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js',
+        'respond'=>'https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js',
     ],
     'permission_deny'=>'你没有操作权限',
 ];
