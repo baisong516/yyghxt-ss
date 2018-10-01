d0691e746434e8e7fa5b29d1b4e34dff43449fc1
chen <1098003693@qq.com>
2018-10-01 09:49:48 +0800

    'cdn'

--- config/yyxt.php
+++ config/yyxt.php
@@ -5,12 +5,12 @@ return [
     'version'=>'V1.0',
     'res'=>[
         //cdn资源
-        'font_awesome'=>'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        'bootstrap_css'=>'https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css',
-        'bootstrap_js'=>'https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js',
-        'jquery'=>'https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js',
+        'font_awesome'=>'/css/font-awesome/css/font-awesome.min.css',
+        'bootstrap_css'=>'/css/bootstrap.min.css',
+        'bootstrap_js'=>'/js/bootstrap.min.js',
+        'jquery'=>'/js/jquery.min.js',
         'admin_css'=>'https://ssyl.oss-cn-shenzhen.aliyuncs.com/res/css/AdminLTE.min.css',
-        'admin_js'=>'https://cdn.bootcss.com/admin-lte/2.4.2/js/adminlte.min.js',
+        'admin_js'=>'/js/adminlte.min.js',
         //主题皮肤
         'admin_skin'=>'https://cdn.bootcss.com/admin-lte/2.4.2/css/skins/skin-green.min.css',
         'app_skin'=>'skin-green',
